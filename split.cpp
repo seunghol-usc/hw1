fc200ac1c9846b3c058fe2208bab1d381c9d57d2
Seungho Lee <seunghol@usc.edu>
2025-09-03 19:21:00 +0000

    part1

--- split.cpp
+++ split.cpp
@@ -4,20 +4,47 @@ CSCI 104: Homework 1 Problem 1
 Write a recursive function to split a sorted singly-linked
 list into two sorted linked lists, where one has the even 
 numbers and the other contains the odd numbers. Students 
-will receive no credit for non-recursive solutions. 
+will receive no credit non-recursive solutions. 
 To test your program write a separate .cpp file and #include
 split.h.  **Do NOT add main() to this file**.  When you submit
 the function below should be the only one in this file.
 */
-
+#include <iostream>
 #include "split.h"
 
-/* Add a prototype for a helper function here if you need */
+using namespace std;
+
+/* Add a prototype a helper function here if you need */
+void append(Node*& n, int val);
+
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+  if (in == NULL) {
+    return;
+  }
+  int x = in->value;
+  if (x%2) {
+    append(odds, x);
+  }else {
+    append(evens, x);
+  }
+
+  //move to next node
+  Node* tmp = in;
+  in = in->next;
+  delete tmp;
+
+  split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
+void append(Node*& n, int val) {
+  if (n == NULL) {
+    n = new Node(val, NULL);
+  }else {
+    Node* tmp = n;
+    n = new Node(val, tmp);
+  }
+}
--- test_split.cpp
+++ test_split.cpp
@@ -8,10 +8,45 @@ Create linked lists and split them with your split() function.
 You can compile this file like this:
 g++ split.cpp test_split.cpp -o test_split
 */
-
+#include <iostream>
 #include "split.h"
+
+using namespace std;
+
+void append(Node*& n, int val);
+void split(Node*& in, Node*& odds, Node*& evens);
+// void print_list(Node* n);
 
 int main(int argc, char* argv[])
 {
+  Node* in = new Node(5, NULL);
+  for (int i = 4; i > 0; i--) {
+    Node* tmp = in;
+    in = new Node(i, tmp);
+  }
 
+  Node* odds = NULL;
+  Node* evens = NULL;
+  split(in, odds, evens);
+
+  // cout << "in = ";
+  // print_list(in);
+
+  // cout << "odds = ";
+  // print_list(odds);
+
+  // cout << "evens = ";
+  // print_list(evens);
+
+  while (odds != NULL) {
+    Node* tmp = odds;
+    odds = odds->next;
+    delete tmp;
+  }
+
+  while (evens != NULL) {
+    Node* tmp = evens;
+    evens = evens->next;
+    delete tmp;
+  }
 }
